f938c77a80a959684375137bea7593ac634ff988
EituPaul <96836425+EituPaul@users.noreply.github.com>
2022-05-10 01:38:34 +0600

    Add files via upload

---
+++ DepositSlot.java
@@ -0,0 +1,11 @@
+
+package atm;
+
+
+public class DepositSlot {
+    
+    public boolean isEnvelopeReceived() {
+        return true;
+    }
+    
+}
---
+++ Screen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+public class Screen {
+    public void displayMsg(String message) {
+        System.out.println(message);
+    }
+    
+    public void displayMsgLine(String message) {
+        System.out.println(message);
+    }
+    
+    public void displayDollarAmount(double amount) {
+        System.out.printf("$%,.2f", amount);
+    }
+    
+}
